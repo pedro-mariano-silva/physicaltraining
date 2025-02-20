2afb33646fbb884797544779afa0a659f2aad45d
Pedro Henrique Mariano <114117512+pedro-mariano-silva@users.noreply.github.com>
2025-02-20 11:09:52 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/pages/login';
+import Home from './src/pages/home'
+import Checkin from './src/pages/checkin'
+import {useFonts} from 'expo-font'
+
+
+
+export default function App() {
+  return (
+         <Home/>
+   
+
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
